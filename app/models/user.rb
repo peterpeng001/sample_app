55bae12851196d84f8262be5f28cb5a6d0174417
peter <peter@peters-MacBook-Pro.local>
2013-02-19 22:35:19 +0800

    Finish signin and signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,14 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   attr_accessor :password, :password_confirmation
   before_save {|user| user.email = email.downcase}
+  before_save :create_remember_token
   validates :name, presence:true,length:{maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence:true,format:{with:VALID_EMAIL_REGEX},
@@ -21,5 +23,16 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def authenticate(password)
+    true
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
+
 
 end
---
+++ db/migrate/20130217143257_add_remember_token_again.rb
@@ -0,0 +1,5 @@
+class AddRememberTokenAgain < ActiveRecord::Migration
+  def change
+    add_column :users, :remember_token, :string
+  end
+end
